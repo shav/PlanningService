87f55e62be40743957ea8678f510ec2a228c7034
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-05 01:04:06 +0400

    Empty WebAPI for PlanningService

---
+++ Planning/MicroSungero.Planning.WebAPI/Controllers/TodoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MicroSungero.Kernel.API;
+
+namespace MicroSungero.Planning.WebAPI.Controllers
+{
+  [Produces("application/json")]
+  [ApiController]
+  [Route("[controller]")]
+  public class TodoController : ControllerBase
+  {
+    private readonly IQueryService queryService;
+
+    private readonly ICommandService commandService;
+
+    public TodoController(IQueryService queryService, ICommandService commandService)
+    {
+      this.queryService = queryService;
+      this.commandService = commandService;
+    }
+  }
+}
