11c97135d5081e2e615702ddc20c0af0ca716d13
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-05 01:37:46 +0400

    Planning module info

---
+++ Planning/Module/MicroSungero.Planning.Module/PlanningModule.cs
@@ -0,0 +1,20 @@
+﻿using MicroSungero.ModuleInfo;
+
+namespace MicroSungero.Planning
+{
+  /// <summary>
+  /// Planning module info.
+  /// </summary>
+  public class Module : ModuleBase
+  {
+    /// <summary>
+    /// Module name.
+    /// </summary>
+    public const string Name = "Planning";
+
+    /// <summary>
+    /// Module assemblies names.
+    /// </summary>
+    public static readonly ModuleAssemblyNames AssemblyNames = new ModuleAssemblyNames(Name);
+  }
+}
