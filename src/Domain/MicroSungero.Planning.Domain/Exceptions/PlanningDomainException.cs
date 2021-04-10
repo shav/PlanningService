3c89b2b32bf555cf0194c184e4e7114f84ee417d
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-10 16:18:56 +0400

    PlanningDomainException

---
+++ src/Domain/MicroSungero.Planning.Domain/Exceptions/PlanningDomainException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MicroSungero.Kernel.Domain.Exceptions;
+
+namespace MicroSungero.Planning.Domain.Exceptions
+{
+  /// <summary>
+  /// Base domain exception for planning module.
+  /// </summary>
+  public class PlanningDomainException : DomainException
+  {
+    /// <summary>
+    /// Create domain exception for planning module.
+    /// </summary>
+    public PlanningDomainException()
+    {
+    }
+
+    /// <summary>
+    /// Create domain exception for planning module.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public PlanningDomainException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Create domain exception for planning module.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Original exception.</param>
+    public PlanningDomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+  }
+}
