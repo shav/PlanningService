4adf3912533dae057ec6ef73c0885806df001012
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-11 15:56:16 +0400

    Todo item priority

---
+++ src/Domain/MicroSungero.Planning.Domain.Entities/TodoList@Todo/TodoPriority.cs
@@ -0,0 +1,33 @@
+﻿using MicroSungero.Kernel.Domain;
+
+namespace MicroSungero.Planning.Domain.Entities
+{
+  /// <summary>
+  /// Todo item priority.
+  /// </summary>
+  public class TodoPriority : Enumeration, ITodoPriority
+  {
+    #region Enumeration items
+
+    public static readonly TodoPriority Low = new TodoPriority(nameof(Low));
+
+    public static readonly TodoPriority Normal = new TodoPriority(nameof(Normal));
+
+    public static readonly TodoPriority High = new TodoPriority(nameof(High));
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Create todo item priority.
+    /// </summary>
+    /// <param name="value">Priority value.</param>
+    public TodoPriority(string value)
+      : base(value)
+    {
+    }
+
+    #endregion
+  }
+}
