ac83353cc92b8cc01854fbe30ed455cf0b5d5eb8
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-11 14:03:34 +0400

    Database context of Planning module.

---
+++ src/Data/MicroSungero.Planning.Data.EntityFramework/PlanningDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MicroSungero.Kernel.Data.EntityFramework;
+using MicroSungero.Kernel.Data;
+
+namespace MicroSungero.Planning.Data.EntityFramework
+{
+  /// <summary>
+  /// Database context of Planning module.
+  /// </summary>
+  public class PlanningDbContext: BaseDbContext
+  {
+    #region DbContext
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Create database context of Planning module.
+    /// </summary>
+    /// <param name="options">Options.</param>
+    /// <param name="connectionSettings">Database connection settings.</param>
+    public PlanningDbContext(DbContextOptions<PlanningDbContext> options, IDatabaseConnectionSettings connectionSettings = null)
+      : base(options, connectionSettings)
+    {
+    }
+
+    #endregion
+  }
+}
