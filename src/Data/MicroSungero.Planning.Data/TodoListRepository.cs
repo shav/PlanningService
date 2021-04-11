7eac89b7b34212fe9c16ac5a8bdbdec5fd445fa3
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-11 13:51:54 +0400

    TodoList repository

---
+++ src/Data/MicroSungero.Planning.Data/TodoListRepository.cs
@@ -0,0 +1,25 @@
+﻿using MicroSungero.Kernel.Data;
+using MicroSungero.Planning.Domain.Entities;
+
+namespace MicroSungero.Planning.Data
+{
+  /// <summary>
+  /// TodoList repository.
+  /// </summary>
+  public class TodoListRepository
+    : BaseEntityInterfaceRepository<TodoList, ITodoList>, IEntityRepository<ITodoList>
+  {
+    #region Constructors
+
+    /// <summary>
+    /// Create TodoList repository.
+    /// </summary>
+    /// <param name="unitOfWorkContext">Unit-of-work context.</param>
+    public TodoListRepository(IUnitOfWorkContext unitOfWorkContext)
+      : base(unitOfWorkContext)
+    {
+    }
+
+    #endregion
+  }
+}
